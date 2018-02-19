79a1b147848b4f52438a8ac63a6438e1617d5122
freedomcuautli <erik.alvarado.pl@gmail.com>
2018-02-18 19:58:44 -0700

    first commit

---
+++ nheqminer/build/CMakeFiles/nheqminer.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/trompequihash/blake2-ref/blake2b-ref.c" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/trompequihash/blake2-ref/blake2b-ref.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "BOOST_ALL_DYN_LINK"
+  "BOOST_ALL_NO_LIB"
+  "BOOST_LOG_DYN_LINK"
+  "__NONINTEL__"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/amount.cpp" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/amount.cpp.o"
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/api.cpp" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/api.cpp.o"
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/arith_uint256.cpp" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/arith_uint256.cpp.o"
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/crypto/ripemd160.cpp" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/crypto/ripemd160.cpp.o"
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/crypto/sha256.cpp" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/crypto/sha256.cpp.o"
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/json/json_spirit_reader.cpp" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/json/json_spirit_reader.cpp.o"
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/json/json_spirit_value.cpp" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/json/json_spirit_value.cpp.o"
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/json/json_spirit_writer.cpp" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/json/json_spirit_writer.cpp.o"
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/libstratum/StratumClient.cpp" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/libstratum/StratumClient.cpp.o"
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/libstratum/ZcashStratum.cpp" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/libstratum/ZcashStratum.cpp.o"
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/main.cpp" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/main.cpp.o"
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/primitives/block.cpp" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/primitives/block.cpp.o"
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/speed.cpp" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/speed.cpp.o"
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/uint256.cpp" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/uint256.cpp.o"
+  "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/utilstrencodings.cpp" "/home/tinkerboard/altnheqminer/nheqminer/nheqminer/build/CMakeFiles/nheqminer.dir/utilstrencodings.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_DYN_LINK"
+  "BOOST_ALL_NO_LIB"
+  "BOOST_LOG_DYN_LINK"
+  "__NONINTEL__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ nheqminer/build/CMakeFiles/nheqminer.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nheqminer.dir/main.cpp.o"
+  "CMakeFiles/nheqminer.dir/amount.cpp.o"
+  "CMakeFiles/nheqminer.dir/api.cpp.o"
+  "CMakeFiles/nheqminer.dir/arith_uint256.cpp.o"
+  "CMakeFiles/nheqminer.dir/crypto/ripemd160.cpp.o"
+  "CMakeFiles/nheqminer.dir/crypto/sha256.cpp.o"
+  "CMakeFiles/nheqminer.dir/json/json_spirit_reader.cpp.o"
+  "CMakeFiles/nheqminer.dir/json/json_spirit_value.cpp.o"
+  "CMakeFiles/nheqminer.dir/json/json_spirit_writer.cpp.o"
+  "CMakeFiles/nheqminer.dir/libstratum/StratumClient.cpp.o"
+  "CMakeFiles/nheqminer.dir/libstratum/ZcashStratum.cpp.o"
+  "CMakeFiles/nheqminer.dir/primitives/block.cpp.o"
+  "CMakeFiles/nheqminer.dir/speed.cpp.o"
+  "CMakeFiles/nheqminer.dir/trompequihash/blake2-ref/blake2b-ref.c.o"
+  "CMakeFiles/nheqminer.dir/uint256.cpp.o"
+  "CMakeFiles/nheqminer.dir/utilstrencodings.cpp.o"
+  "nheqminer.pdb"
+  "nheqminer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/nheqminer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
